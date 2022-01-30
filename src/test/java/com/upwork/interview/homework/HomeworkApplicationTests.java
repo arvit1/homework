83c98a2402c6ef19cae12c2f80a5548f527305e7
Arvit Varfaj <avarfaj86@gmail.com>
2022-01-30 20:55:30 +0100

    test checkin

--- src/test/java/com/upwork/interview/homework/HomeworkApplicationTests.java
+++ src/test/java/com/upwork/interview/homework/HomeworkApplicationTests.java
@@ -1,63 +1,53 @@
 package com.upwork.interview.homework;
 
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.StandardCharsets;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
+@AutoConfigureMockMvc
 class HomeworkApplicationTests {
+    @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    protected WebApplicationContext wac;
-
-    @Autowired
-    private FilterChainProxy springSecurityFilterChain;
-
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
-                .apply(springSecurity(springSecurityFilterChain)).build();
-    }
 
     @DisplayName("Test checkin id student is already checked in")
     @Test
     void testCheckIn() throws Exception {
+        String expectedString = "{\"activityId\":1,\"name\":\"Math\",\"start\":\"2022-02-04T10:00:00\",\"end\":\"2022-02-04T10:50:00\",\"students\":[{\"studentId\":3,\"fullName\":\"John Watson\",\"username\":\"john\",\"password\":\"$2a$10$E2UPv7arXmp3q0LzVzCBNeb4B4AtbTAGjkefVDnSztOwE7Gix6kea\",\"enabled\":true},{\"studentId\":2,\"fullName\":\"Admin Me\",\"username\":\"admin\",\"password\":\"$2a$10$IqTJTjn39IU5.7sSCDQxzu3xug6z/LPU6IF0azE/8CkHCwYEnwBX.\",\"enabled\":true},{\"studentId\":1,\"fullName\":\"Patrick Pwayze\",\"username\":\"patrick\",\"password\":\"$2a$10$cTUErxQqYVyU2qmQGIktpup5chLEdhD2zpzNEyYqmxrHHJbSNDOG.\",\"enabled\":true}]}";
+
         mockMvc.perform(get("/api/checkin?activityId=" + 1)
                 .with(httpBasic("admin", "admin"))
                 .contentType(new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8)))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.students", Matchers.hasSize(2)));
+                .andExpect(content().json(expectedString));
     }
 
     @DisplayName("Test checkin id student is added")
     @Test
     void testCheckInPerform() throws Exception {
+        String expectedString = "{\"activityId\":1,\"name\":\"Math\",\"start\":\"2022-02-04T10:00:00\",\"end\":\"2022-02-04T10:50:00\",\"students\":[{\"studentId\":3,\"fullName\":\"John Watson\",\"username\":\"john\",\"password\":\"$2a$10$E2UPv7arXmp3q0LzVzCBNeb4B4AtbTAGjkefVDnSztOwE7Gix6kea\",\"enabled\":true},{\"studentId\":2,\"fullName\":\"Admin Me\",\"username\":\"admin\",\"password\":\"$2a$10$IqTJTjn39IU5.7sSCDQxzu3xug6z/LPU6IF0azE/8CkHCwYEnwBX.\",\"enabled\":true},{\"studentId\":1,\"fullName\":\"Patrick Pwayze\",\"username\":\"patrick\",\"password\":\"$2a$10$cTUErxQqYVyU2qmQGIktpup5chLEdhD2zpzNEyYqmxrHHJbSNDOG.\",\"enabled\":true}]}";
+
         mockMvc.perform(get("/api/checkin?activityId=" + 1)
                 .with(httpBasic("john", "john"))
                 .contentType(new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8)))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.students", Matchers.hasSize(3)));
+                .andExpect(content().json(expectedString));
     }
 
 }
