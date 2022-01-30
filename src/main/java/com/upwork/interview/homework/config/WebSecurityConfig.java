ff520de54e939ca16300b216313a042c98e75419
Arvit Varfaj <avarfaj86@gmail.com>
2022-01-30 13:21:58 +0100

    get activities order by date asc and in the future

--- src/main/java/com/upwork/interview/homework/config/WebSecurityConfig.java
+++ src/main/java/com/upwork/interview/homework/config/WebSecurityConfig.java
@@ -42,11 +42,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
+//                .httpBasic()
+//                .and()
                 .formLogin().permitAll()
                 .and()
                 .logout().permitAll()
                 .and()
                 .exceptionHandling().accessDeniedPage("/login?error=true")
+//                .and()
+//                .csrf().disable().cors()
         ;
     }
 }
